b8c7eb7ca915449b7eea8554431ce53a18ea4bc7
DaivarasDev <daivarasdev@gmail.com>
2021-04-10 14:44:40 +0200

    Uploading project to GitHub

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Pop-Up Modal
+const modal = document.getElementById("popUp-Modal");
+// Get the button element that closes the modal
+const modalButton = document.getElementById("popUp-ModalButton");
+// Close the modal once the button is pressed
+modalButton.addEventListener("click", function () {
+  if (typeof modal.style !== "undefined") {
+    modal.classList.add("modal-content-active");
+    setTimeout(function () {
+      modal.classList.remove("modal-content-active");
+      modal.style.display = "none";
+    }, 150);
+  }
+});
+
+// If user clicks anywhere outside of the window, close it
+window.onclick = function (event) {
+  if (event.target.classList.contains("modal")) {
+    if (typeof modal.style !== "undefined") {
+      modal.classList.add("modal-content-active");
+      setTimeout(function () {
+        modal.classList.remove("modal-content-active");
+        modal.style.display = "none";
+      }, 150);
+    }
+  }
+};
